0598824bcd702824a50d7dae2816e56c67430a4c
Pritam Saha <saha.pritam.work@gmail.com>
2024-05-28 10:10:25 +0530

    Added basic controller, command liner runner, datasource setup

---
+++ rest/src/main/java/com/example/rest/CommandLinerRunnerImpl.java
@@ -0,0 +1,13 @@
+package com.example.rest;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class CommandLinerRunnerImpl implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
