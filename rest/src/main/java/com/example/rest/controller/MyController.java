bd93b89a7d03ec7cacdee9c89fefc514646a07be
Agniva Paul <paulagniva017@gmail.com>
2024-05-28 11:49:46 +0530

    added /teachers/{id} endpoint

--- rest/src/main/java/com/example/rest/controller/MyController.java
+++ rest/src/main/java/com/example/rest/controller/MyController.java
@@ -1,5 +1,20 @@
 package com.example.rest.controller;
 
-public class MyController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.rest.entity.Teacher;
+import com.example.rest.repository.TeacherRepository;
+
+@RestController
+public class MyController {
+	@Autowired
+	private TeacherRepository teacherRepository;
+	
+	@GetMapping("/teachers/{id}")
+	public Teacher getTeacherById(@PathVariable int id) {
+		return teacherRepository.findById(id).orElse(null);
+	}
 }
