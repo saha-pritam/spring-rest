9723daa35e6152b76a676920414f1368f9d9e729
Agniva Paul <paulagniva017@gmail.com>
2024-05-28 11:29:35 +0530

    Added some dummy data in teachers table

--- rest/src/main/java/com/example/rest/CommandLinerRunnerImpl.java
+++ rest/src/main/java/com/example/rest/CommandLinerRunnerImpl.java
@@ -5,12 +5,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.example.rest.entity.Student;
+import com.example.rest.entity.Teacher;
 import com.example.rest.repository.StudentRepository;
+import com.example.rest.repository.TeacherRepository;
 @Component
 public class CommandLinerRunnerImpl implements CommandLineRunner {
 	
 	@Autowired
 	private StudentRepository studentRepository; 
+	
+	@Autowired
+	private TeacherRepository teacherRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -18,6 +23,10 @@ public class CommandLinerRunnerImpl implements CommandLineRunner {
 		studentRepository.save(new Student("Agniva","Paul"));
 		studentRepository.save(new Student("Nilesh","Roy Chowdhury"));
 		studentRepository.save(new Student("Shitij","Bhadra"));
+		
+		teacherRepository.save(new Teacher("Pritam","Saha"));
+		teacherRepository.save(new Teacher("Amalesh","Pradhan"));
+		teacherRepository.save(new Teacher("Ankur","Chakraborty"));
 
 	}
 
